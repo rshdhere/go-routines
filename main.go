8360bfb2dc972af8c0f95b7251a4554d0e79de04
raashed <mohammedraashed10@gmail.com>
2025-11-09 10:00:08 +0530

    fix(main): correct string typo in print statements - Fix typo in first print statement from "ths" to "the" - Fix typo in second print statement from "ths" to "the" - Improve code structure by adding a dedicated print function - Introduce goroutine for asynchronous printing of the first message

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func printSomething(s string) {
+	fmt.Println(s)
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	go printSomething("This is ths first thing to be printed")
+
+	time.Sleep(1 * time.Second)
+
+	printSomething("This is ths second thing to be printed")
 }
